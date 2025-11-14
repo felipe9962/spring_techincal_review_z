6d856f964a2cf4b9ef3683a68e487264e608d0f8
Felipe9962 <felipe_casanueva99@outlook.com>
2025-11-14 17:52:33 +0100

    Initial commit

---
+++ src/main/java/com/felipe/spring_techincal_review_z/SpringTechincalReviewZApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.spring_techincal_review_z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechincalReviewZApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTechincalReviewZApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/felipe/spring_techincal_review_z/SpringTechincalReviewZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.spring_techincal_review_z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTechincalReviewZApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
